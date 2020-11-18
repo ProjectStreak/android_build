5cb93e83c4db05e84c4f4ba5095064c8e8142e51
kubersharma001 <kubersharma001@gmail.com>
2020-11-18 05:54:28 +0100

    core: config: Include streak sepolicy little earlier.
    * streak sepolicy is now considered
    
    Change-Id: If0cfebafa0c3b3dc52434fca7798a82076ed387a

--- core/config.mk
+++ core/config.mk
@@ -1162,6 +1162,14 @@ dont_bother_goals := out \
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
+ifneq ($(STREAK_BUILD),)
+ifneq ($(wildcard device/streak/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/streak/sepolicy/common/sepolicy.mk)
+endif
+endif
+
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
@@ -1174,14 +1182,6 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 
 # Make RECORD_ALL_DEPS readonly.
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))
-
-ifneq ($(STREAK_BUILD),)
-ifneq ($(wildcard device/streak/sepolicy/common/sepolicy.mk),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/streak/sepolicy/common/sepolicy.mk)
-endif
-endif
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk

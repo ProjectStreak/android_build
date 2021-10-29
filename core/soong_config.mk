962a1748d06f57c54a696e66ede0bc82170aaf93
Harshit Jain <harshit@hyperteam.co.in>
2021-10-29 20:58:19 +0000

    core: soong_config: add new flag for udfps extension lib
    Change-Id: Ief208da819c10bb81780805ff529dc795cc62f62

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -271,6 +271,8 @@ $(call add_json_bool, SepolicySplit, $(filter true,$(PRODUCT_SEPOLICY_SPLIT)))
 
 $(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
 
+$(call add_json_str_omitempty, Target_surfaceflinger_udfps_lib, $(TARGET_SURFACEFLINGER_UDFPS_LIB))
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))
